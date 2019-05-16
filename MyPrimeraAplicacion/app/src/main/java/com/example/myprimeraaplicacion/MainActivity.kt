75b9b23f93f142a3795ef3a4da6a9fe7ebe78d9b
CaroCe <carito_kt@hotmail.com>
2019-05-16 16:06:34 -0500

    clase Intent

--- MyPrimeraAplicacion/app/src/main/java/com/example/myprimeraaplicacion/MainActivity.kt
+++ MyPrimeraAplicacion/app/src/main/java/com/example/myprimeraaplicacion/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myprimeraaplicacion
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +21,22 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        btn_actividad_dos.setOnClickListener {
+            //this.irAActividadDos()-> se puede omitir el this
+            irAActividadDos()
+        }
+    }
+
+    fun irAActividadDos(){
+        //se crea una instancia de la clase intent
+
+        val intent = Intent(
+            this,
+            ActividadDos::class.java
+        )
+        startActivity(intent)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
